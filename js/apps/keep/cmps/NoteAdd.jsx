2090c4f90bb802dfcd4f9de34b3d4bd415a8960a
RazAbk <RazAb98754@Gmail.com>
2021-08-26 23:18:53 +0300

    noteAdd is now a working seperate component

--- js/apps/keep/cmps/NoteAdd.jsx
+++ js/apps/keep/cmps/NoteAdd.jsx
@@ -1,8 +1,78 @@
-export function NoteAdd(){
+export function NoteAdd({inputType, setInputType}){
+
+
+    const formRef = React.createRef();
+    const functionBtns = React.createRef();
+    const secondInput = React.createRef();
+    const addNote = React.createRef();
+
+    const onEnterForm = (ev) => {
+        // Expand
+        const form = ev.target.form;
+        form.classList.add('expand-form')
+
+        //Reveal function buttons
+        functionBtns.current.classList.add('inside-form-visible');
+        // Reveal second input
+        if(inputType !== 'text'){
+            secondInput.current.classList.add('inside-form-visible');
+        }
+        // Reveal add button
+        addNote.current.classList.add('inside-form-visible');
+    }
+
+    const onLeaveForm = (ev) => {
+            // console.log(ev.target.id) 
+            // Shrink
+            const form = ev.target.form;
+            form.classList.remove('expand-form')
+            
+            // UnReveal function buttons
+            functionBtns.current.classList.remove('inside-form-visible');
+            // UnReveal second input
+            secondInput.current.classList.remove('inside-form-visible');
+            // UnReveal add button
+            addNote.current.classList.remove('inside-form-visible');
+    }
+
+    const onSetInputType = (type) => {
+        setInputType(type);
+
+        // Expand
+        formRef.current.classList.add('expand-form')
+
+        //Reveal function buttons
+        functionBtns.current.classList.add('inside-form-visible');
+        // Reveal second input
+        if(type !== 'text'){
+            secondInput.current.classList.add('inside-form-visible');
+        }else{
+            secondInput.current.classList.remove('inside-form-visible');
+        }
+        // Reveal add button
+        addNote.current.classList.add('inside-form-visible');
+
+    }
 
 
 
     return (
-        <h1>1</h1>
+        <section className="notes-input notes-layout">
+            <form className="notes-input-form" ref={formRef}>
+                <label htmlFor="notes-title"></label>
+                <input id="notes-title" type="text" placeholder="What's on your mind..." onFocus={onEnterForm} onBlur={onLeaveForm}/>
+                <div className={`second-input ${inputType !== 'text' ? 'inside-form-visible' : ''}`} ref={secondInput}>
+                    <label htmlFor="notes-content"></label>
+                    <input   id="notes-content" type="text" placeholder={(inputType === 'todo' ? 'Let\'s make some tasks (use , to separate)' : 'Url goes here')} onFocus={onEnterForm} onBlur={onLeaveForm}/>
+                </div>
+                <div className="choose-type-btns" ref={functionBtns}>
+                    <i tabIndex="-1" onFocus={(ev)=>{ev.target.click()}} onBlur={(ev)=>{ev.target.blur()}} onClick={() => {onSetInputType('text')}} style={{color: (inputType === 'text' ? '#efc718' : 'black')}} className="far fa-file-alt"></i>
+                    <i tabIndex="-1" onFocus={(ev)=>{ev.target.click()}} onBlur={(ev)=>{ev.target.blur()}} onClick={() => {onSetInputType('todo')}} style={{color: (inputType === 'todo' ? '#efc718' : 'black')}} className="fas fa-list-ul"></i>
+                    <i tabIndex="-1" onFocus={(ev)=>{ev.target.click()}} onBlur={(ev)=>{ev.target.blur()}} onClick={() => {onSetInputType('image')}} style={{color: (inputType === 'image' ? '#efc718' : 'black')}} className="far fa-image"></i>
+                    <i tabIndex="-1" onFocus={(ev)=>{ev.target.click()}} onBlur={(ev)=>{ev.target.blur()}} onClick={() => {onSetInputType('video')}} style={{color: (inputType === 'video' ? 'red' : 'black')}} className="fab fa-youtube"></i>
+                </div>
+                <button tabIndex="-1" onFocus={(ev)=>{ev.target.click()}} onBlur={(ev)=>{ev.target.blur()}} onClick={() => {console.log('add note')}} ref={addNote} className="add-note-btn">add note</button>
+            </form>
+        </section>
     )
 }
--- js/apps/keep/pages/NotesApp.jsx
+++ js/apps/keep/pages/NotesApp.jsx
@@ -3,19 +3,13 @@ import { NotesList } from "../cmps/NotesList.jsx";
 import { NoteAdd } from "../cmps/NoteAdd.jsx";
 
 export class NotesApp extends React.Component {
-
     state = {
         notes: [],
         inputType: 'text'
     }
 
-
     componentDidMount(){
         this.loadNotes();
-        this.formRef = React.createRef();
-        this.functionBtns = React.createRef();
-        this.secondInput = React.createRef();
-        this.addNote = React.createRef();
     }
 
     loadNotes = () => {
@@ -24,54 +18,10 @@ export class NotesApp extends React.Component {
         })
     }
 
-    onEnterForm = (ev) => {
-        // Expand
-        const form = ev.target.form;
-        form.classList.add('expand-form')
-
-        //Reveal function buttons
-        this.functionBtns.current.classList.add('inside-form-visible');
-        // Reveal second input
-        if(this.state.inputType !== 'text'){
-            this.secondInput.current.classList.add('inside-form-visible');
-        }
-        // Reveal add button
-        this.addNote.current.classList.add('inside-form-visible');
-    }
-
-    onLeaveForm = (ev) => {
-            // console.log(ev.target.id) 
-            // Shrink
-            const form = ev.target.form;
-            form.classList.remove('expand-form')
-            
-            // UnReveal function buttons
-            this.functionBtns.current.classList.remove('inside-form-visible');
-            // UnReveal second input
-            this.secondInput.current.classList.remove('inside-form-visible');
-            // UnReveal add button
-            this.addNote.current.classList.remove('inside-form-visible');
-    }
-
-    onSetInputType = (type) => {
+    setInputType = (type) => {
         this.setState({...this.state, inputType: type})
-
-        // Expand
-        this.formRef.current.classList.add('expand-form')
-
-        //Reveal function buttons
-        this.functionBtns.current.classList.add('inside-form-visible');
-        // Reveal second input
-        if(type !== 'text'){
-            this.secondInput.current.classList.add('inside-form-visible');
-        }else{
-            this.secondInput.current.classList.remove('inside-form-visible');
-        }
-        // Reveal add button
-        this.addNote.current.classList.add('inside-form-visible');
-
     }
-
+   
     // Crud
 
     onDeleteNote = (noteId) => {
@@ -96,23 +46,7 @@ export class NotesApp extends React.Component {
         return(
             <div className="notes-app">
 
-                <section className="notes-input notes-layout">
-                    <form className="notes-input-form" ref={this.formRef}>
-                        <label htmlFor="notes-title"></label>
-                        <input id="notes-title" type="text" placeholder="Whats on your mind?" onFocus={this.onEnterForm} onBlur={this.onLeaveForm}/>
-                        <div className={`second-input ${inputType !== 'text' ? 'inside-form-visible' : ''}`} ref={this.secondInput}>
-                            <label htmlFor="notes-content"></label>
-                            <input   id="notes-content" type="text" placeholder="Take a note" onFocus={this.onEnterForm} onBlur={this.onLeaveForm}/>
-                        </div>
-                        <div className="choose-type-btns" ref={this.functionBtns}>
-                            <i tabIndex="-1" onFocus={(ev)=>{ev.target.click()}} onBlur={(ev)=>{ev.target.blur()}} onClick={() => {this.onSetInputType('text')}} className="far fa-file-alt"></i>
-                            <i tabIndex="-1" onFocus={(ev)=>{ev.target.click()}} onBlur={(ev)=>{ev.target.blur()}} onClick={() => {this.onSetInputType('todo')}} className="fas fa-list-ul"></i>
-                            <i tabIndex="-1" onFocus={(ev)=>{ev.target.click()}} onBlur={(ev)=>{ev.target.blur()}} onClick={() => {this.onSetInputType('image')}} className="far fa-image"></i>
-                            <i tabIndex="-1" onFocus={(ev)=>{ev.target.click()}} onBlur={(ev)=>{ev.target.blur()}} onClick={() => {this.onSetInputType('video')}} className="fab fa-youtube"></i>
-                        </div>
-                        <button tabIndex="-1" onFocus={(ev)=>{ev.target.click()}} onBlur={(ev)=>{ev.target.blur()}} onClick={() => {console.log('add note')}} ref={this.addNote} className="add-note-btn">add note</button>
-                    </form>
-                </section>
+               <NoteAdd inputType={inputType} setInputType={this.setInputType} />
 
                 <section className="notes-cards notes-layout">
                     <h2>pinned</h2>
@@ -125,7 +59,7 @@ export class NotesApp extends React.Component {
                                 onDuplicateNote={this.onDuplicateNote}
                             />
                         </div>
-                        </div>
+                    </div>
 
                     <h2>notes</h2>
                     <div className="notes-general">
