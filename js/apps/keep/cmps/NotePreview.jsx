0517fac73d75388f0ae952548c303fd56d7dd74d
Guy snir <guy.snir97@gmail.com>
2021-08-26 14:20:00 +0300

    commit

--- js/apps/keep/cmps/NotePreview.jsx
+++ js/apps/keep/cmps/NotePreview.jsx
@@ -1,10 +1,14 @@
-export function NotePreview({ note }){
+import { NoteImg } from "./NoteImg.jsx";
+import { NotesList } from "./NotesList";
 
-    const {info} = note;
+export function NotePreview({ note }) {
 
-    return(
+    const { info } = note;
+
+    return (
         <div className="note">
-            {Object.keys(info).map((inf,idx) => <h2 key={`${note.id}-${idx}`}>{inf}</h2>)}
+            {Object.keys(info).map((inf, idx) => <h2 key={`${note.id}-${idx}`}>{inf}</h2>)}
+            <NoteImg note={note} />
         </div>
     )
 }
