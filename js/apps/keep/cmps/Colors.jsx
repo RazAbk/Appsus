6f8f7eb89a0cd57e30fe8cd18b05f3d88703ad32
RazAbk <RazAb98754@Gmail.com>
2021-08-29 06:06:05 +0300

    note colors edit updated

--- js/apps/keep/cmps/Colors.jsx
+++ js/apps/keep/cmps/Colors.jsx
@@ -1,13 +1,12 @@
 export function Colors({noteId, onChangeColor}) {
-
-
     return(
-
         <section className={`colors colors-${noteId}`}>
-            <button onClick={()=>{onChangeColor('blue')}}>blue</button>
-            <button onClick={()=>{onChangeColor('red')}}>red</button>
+            <button onClick={()=>{onChangeColor('#866dda')}} style={{backgroundColor: '#866dda'}}></button>
+            <button onClick={()=>{onChangeColor('#da6666ed')}} style={{backgroundColor: '#da6666ed'}}></button>
+            <button onClick={()=>{onChangeColor('#3daf55')}} style={{backgroundColor: '#3daf55'}}></button>
+            <button onClick={()=>{onChangeColor('#d5d845')}} style={{backgroundColor: '#d5d845'}}></button>
+            <button onClick={()=>{onChangeColor('#33d2d8')}} style={{backgroundColor: '#33d2d8'}}></button>
         </section>
         )
-
 }
 
